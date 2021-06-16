48179df27d6db8e0d6df6aefc05c4b9974351d7c
omayma hajjami <omayma.hajjami@gmail.com>
2021-06-16 16:59:25 +0100

    add all product with Api

---
+++ prodApi/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using prodApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prodApi.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> Get();
+        Task<Product> Get(int id);
+        Task<Product> Create(Product product);
+        Task Update(Product product);
+        Task Delete(int id);
+    }
+}
---
+++ prodApi/Repositories/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prodApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prodApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _context;
+        public ProductRepository(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task Delete(int id)
+        {
+            var productToDelete = await _context.Products.FindAsync(id);
+            _context.Products.Remove(productToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> Get(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
